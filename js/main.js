affdbdec8befe3ab14578d025ab551c4a9469f61
Luka-dot <79lukask@gmail.com>
2020-11-04 20:15:35 -0500

    burger menu animation for open and closed added

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ $('.burgerIcon').hover(function() {
         TweenMax.to('.burgerLine:last-child', 0.2, { x: 0 });
     }
 ); 
+
+var tlMenu = new TimelineMax({paused: true});
+
+tlMenu.to('nav', 0.3, { autoAlpha: 1 })
+.staggerFromTo('nav li', 0.5, { y: 100, opacity: 0 }, { y: 0, opacity: 1 }, 0.1);
+
+$('.burgerIcon').click(function() {
+    tlMenu.play(0);
+});
+
+$('.closeButton').click(function() {
+    tlMenu.reverse(0);
+});
